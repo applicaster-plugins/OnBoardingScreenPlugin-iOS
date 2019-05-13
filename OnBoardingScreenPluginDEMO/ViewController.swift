9a35c256784f5986d3aa0395a95e7ed816371541
Marcos J Reyes <m.reyes@applicaster.com>
2019-05-13 18:28:23 -0400

    chore: clean-up

--- OnBoardingScreenPluginDEMO/ViewController.swift
+++ OnBoardingScreenPluginDEMO/ViewController.swift
@@ -7,40 +7,8 @@
 //
 
 import UIKit
-import CopaAmericaStatsScreenPlugin
 
 class ViewController: UIViewController {
-    @IBOutlet weak var configurationTextField: UITextField!
-    @IBOutlet weak var parametersTextField: UITextField!
-    var myPlugin: CopaAmericaStatsScreenPlugin?
-    
-    @IBAction func initializePluginClicked(_ sender: Any) {
-        if let data = configurationTextField.text?.data(using: String.Encoding.utf8),
-            let configuration = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
-            myPlugin = CopaAmericaStatsScreenPlugin(nibName: nil, bundle: nil)
-        } else {
-            let alert = UIAlertController(title: nil, message: "Please enter a valid json in the configuration field", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func handleURLSchemeClicked(_ sender: Any) {
-        if let myPlugin = myPlugin {
-            if let data = parametersTextField.text?.data(using: String.Encoding.utf8),
-                let params = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary,
-                let dictionaryParams = params {
-                myPlugin.handleUrlScheme(dictionaryParams)
-            } else {
-                let alert = UIAlertController(title: nil, message: "Please enter a valid json in the configuration field", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-            }
-        } else {
-            let alert = UIAlertController(title: nil, message: "Please first initialize a plugin", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
+
 }
 
--- README.md
+++ README.md
@@ -1,19 +1,1 @@
 # On Boarding Loading Hook Screen Plugin for iOS
-
-## Getting started
-
-TBD
-
-## Configuration
-
-TBD
-
-## Submit an Issue
-
-For submitting issues and bug reports, please use the following link:
-https://github.com/applicaster/CopaAmericaStatsScreenPlugin-iOS/issues/new
-
-## Code of Conduct
-
-Please make sure to follow our code of conduct:
-https://developer-zapp.applicaster.com/Code-Of-Conduct.html
